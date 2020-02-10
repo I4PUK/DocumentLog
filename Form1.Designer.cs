1d3b2266176b402e7624d0aadbb572447ed0bae5
Сергей Мишуков <Mis.Serj@yandex.ru>
2020-02-10 15:47:58 +0300

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace testApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.databaseDataSet = new testApp.DatabaseDataSet();
+            this.documentsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.documentsTableAdapter = new testApp.DatabaseDataSetTableAdapters.DocumentsTableAdapter();
+            this.номерДокументаDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.датаДокументаDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.описаниеDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.подписанDataGridViewCheckBoxColumn = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.databaseDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.номерДокументаDataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.датаДокументаDataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewComboBoxColumn1 = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.подписанDataGridViewCheckBoxColumn1 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.databaseDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.documentsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.databaseDataSetBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // databaseDataSet
+            // 
+            this.databaseDataSet.DataSetName = "DatabaseDataSet";
+            this.databaseDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // documentsBindingSource
+            // 
+            this.documentsBindingSource.DataMember = "Documents";
+            this.documentsBindingSource.DataSource = this.databaseDataSet;
+            // 
+            // documentsTableAdapter
+            // 
+            this.documentsTableAdapter.ClearBeforeFill = true;
+            // 
+            // номерДокументаDataGridViewTextBoxColumn
+            // 
+            this.номерДокументаDataGridViewTextBoxColumn.DataPropertyName = "Номер документа";
+            this.номерДокументаDataGridViewTextBoxColumn.HeaderText = "Номер документа";
+            this.номерДокументаDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.номерДокументаDataGridViewTextBoxColumn.Name = "номерДокументаDataGridViewTextBoxColumn";
+            this.номерДокументаDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // датаДокументаDataGridViewTextBoxColumn
+            // 
+            this.датаДокументаDataGridViewTextBoxColumn.DataPropertyName = "Дата документа";
+            this.датаДокументаDataGridViewTextBoxColumn.HeaderText = "Дата документа";
+            this.датаДокументаDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.датаДокументаDataGridViewTextBoxColumn.Name = "датаДокументаDataGridViewTextBoxColumn";
+            this.датаДокументаDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // описаниеDataGridViewTextBoxColumn
+            // 
+            this.описаниеDataGridViewTextBoxColumn.DataPropertyName = "Описание ";
+            this.описаниеDataGridViewTextBoxColumn.HeaderText = "Описание ";
+            this.описаниеDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.описаниеDataGridViewTextBoxColumn.Name = "описаниеDataGridViewTextBoxColumn";
+            this.описаниеDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // подписанDataGridViewCheckBoxColumn
+            // 
+            this.подписанDataGridViewCheckBoxColumn.DataPropertyName = "Подписан";
+            this.подписанDataGridViewCheckBoxColumn.HeaderText = "Подписан";
+            this.подписанDataGridViewCheckBoxColumn.MinimumWidth = 6;
+            this.подписанDataGridViewCheckBoxColumn.Name = "подписанDataGridViewCheckBoxColumn";
+            this.подписанDataGridViewCheckBoxColumn.Width = 125;
+            // 
+            // Column1
+            // 
+            this.Column1.DataPropertyName = "Номер документа";
+            this.Column1.HeaderText = "Номер документа";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.DataPropertyName = "Номер документа";
+            this.Column2.HeaderText = "Column2";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 125;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.номерДокументаDataGridViewTextBoxColumn1,
+            this.датаДокументаDataGridViewTextBoxColumn1,
+            this.dataGridViewComboBoxColumn1,
+            this.подписанDataGridViewCheckBoxColumn1});
+            this.dataGridView1.DataSource = this.documentsBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 425);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // databaseDataSetBindingSource
+            // 
+            this.databaseDataSetBindingSource.DataSource = this.databaseDataSet;
+            this.databaseDataSetBindingSource.Position = 0;
+            // 
+            // номерДокументаDataGridViewTextBoxColumn1
+            // 
+            this.номерДокументаDataGridViewTextBoxColumn1.DataPropertyName = "Номер документа";
+            this.номерДокументаDataGridViewTextBoxColumn1.HeaderText = "Номер документа";
+            this.номерДокументаDataGridViewTextBoxColumn1.MinimumWidth = 6;
+            this.номерДокументаDataGridViewTextBoxColumn1.Name = "номерДокументаDataGridViewTextBoxColumn1";
+            this.номерДокументаDataGridViewTextBoxColumn1.ReadOnly = true;
+            // 
+            // датаДокументаDataGridViewTextBoxColumn1
+            // 
+            this.датаДокументаDataGridViewTextBoxColumn1.DataPropertyName = "Дата документа";
+            this.датаДокументаDataGridViewTextBoxColumn1.HeaderText = "Дата документа";
+            this.датаДокументаDataGridViewTextBoxColumn1.MinimumWidth = 6;
+            this.датаДокументаDataGridViewTextBoxColumn1.Name = "датаДокументаDataGridViewTextBoxColumn1";
+            this.датаДокументаDataGridViewTextBoxColumn1.ReadOnly = true;
+            // 
+            // dataGridViewComboBoxColumn1
+            // 
+            this.dataGridViewComboBoxColumn1.DataPropertyName = "Статус ";
+            this.dataGridViewComboBoxColumn1.HeaderText = "Статус ";
+            this.dataGridViewComboBoxColumn1.MinimumWidth = 6;
+            this.dataGridViewComboBoxColumn1.Name = "dataGridViewComboBoxColumn1";
+            this.dataGridViewComboBoxColumn1.ReadOnly = true;
+            // 
+            // подписанDataGridViewCheckBoxColumn1
+            // 
+            this.подписанDataGridViewCheckBoxColumn1.DataPropertyName = "Подписан";
+            this.подписанDataGridViewCheckBoxColumn1.HeaderText = "Подписан";
+            this.подписанDataGridViewCheckBoxColumn1.MinimumWidth = 6;
+            this.подписанDataGridViewCheckBoxColumn1.Name = "подписанDataGridViewCheckBoxColumn1";
+            this.подписанDataGridViewCheckBoxColumn1.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.databaseDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.documentsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.databaseDataSetBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private DatabaseDataSet databaseDataSet;
+        private System.Windows.Forms.BindingSource documentsBindingSource;
+        private DatabaseDataSetTableAdapters.DocumentsTableAdapter documentsTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn номерДокументаDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn датаДокументаDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn описаниеDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn подписанDataGridViewCheckBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource databaseDataSetBindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn номерДокументаDataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn датаДокументаDataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewComboBoxColumn dataGridViewComboBoxColumn1;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn подписанDataGridViewCheckBoxColumn1;
+    }
+}
+
---
+++ SignForm.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace testApp
+{
+    partial class SignForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label номер_документаLabel;
+            System.Windows.Forms.Label дата_документаLabel;
+            System.Windows.Forms.Label описание_Label;
+            System.Windows.Forms.Label статус_Label;
+            System.Windows.Forms.Label подписанLabel;
+            this.databaseDataSet = new testApp.DatabaseDataSet();
+            this.databaseDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.documentsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.documentsTableAdapter = new testApp.DatabaseDataSetTableAdapters.DocumentsTableAdapter();
+            this.tableAdapterManager = new testApp.DatabaseDataSetTableAdapters.TableAdapterManager();
+            this.номер_документаTextBox = new System.Windows.Forms.TextBox();
+            this.дата_документаDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.описание_TextBox = new System.Windows.Forms.TextBox();
+            this.статус_ComboBox = new System.Windows.Forms.ComboBox();
+            this.подписанCheckBox = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            номер_документаLabel = new System.Windows.Forms.Label();
+            дата_документаLabel = new System.Windows.Forms.Label();
+            описание_Label = new System.Windows.Forms.Label();
+            статус_Label = new System.Windows.Forms.Label();
+            подписанLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.databaseDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.databaseDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.documentsBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // databaseDataSet
+            // 
+            this.databaseDataSet.DataSetName = "DatabaseDataSet";
+            this.databaseDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // databaseDataSetBindingSource
+            // 
+            this.databaseDataSetBindingSource.DataSource = this.databaseDataSet;
+            this.databaseDataSetBindingSource.Position = 0;
+            // 
+            // documentsBindingSource
+            // 
+            this.documentsBindingSource.DataMember = "Documents";
+            this.documentsBindingSource.DataSource = this.databaseDataSet;
+            // 
+            // documentsTableAdapter
+            // 
+            this.documentsTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.DocumentsTableAdapter = this.documentsTableAdapter;
+            this.tableAdapterManager.UpdateOrder = testApp.DatabaseDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // номер_документаLabel
+            // 
+            номер_документаLabel.AutoSize = true;
+            номер_документаLabel.Location = new System.Drawing.Point(214, 82);
+            номер_документаLabel.Name = "номер_документаLabel";
+            номер_документаLabel.Size = new System.Drawing.Size(129, 17);
+            номер_документаLabel.TabIndex = 0;
+            номер_документаLabel.Text = "Номер документа:";
+            // 
+            // номер_документаTextBox
+            // 
+            this.номер_документаTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.documentsBindingSource, "Номер документа", true));
+            this.номер_документаTextBox.Location = new System.Drawing.Point(349, 79);
+            this.номер_документаTextBox.Name = "номер_документаTextBox";
+            this.номер_документаTextBox.Size = new System.Drawing.Size(200, 22);
+            this.номер_документаTextBox.TabIndex = 1;
+            // 
+            // дата_документаLabel
+            // 
+            дата_документаLabel.AutoSize = true;
+            дата_документаLabel.Location = new System.Drawing.Point(214, 111);
+            дата_документаLabel.Name = "дата_документаLabel";
+            дата_документаLabel.Size = new System.Drawing.Size(120, 17);
+            дата_документаLabel.TabIndex = 2;
+            дата_документаLabel.Text = "Дата документа:";
+            // 
+            // дата_документаDateTimePicker
+            // 
+            this.дата_документаDateTimePicker.DataBindings.Add(new System.Windows.Forms.Binding("Value", this.documentsBindingSource, "Дата документа", true));
+            this.дата_документаDateTimePicker.Location = new System.Drawing.Point(349, 107);
+            this.дата_документаDateTimePicker.Name = "дата_документаDateTimePicker";
+            this.дата_документаDateTimePicker.Size = new System.Drawing.Size(200, 22);
+            this.дата_документаDateTimePicker.TabIndex = 3;
+            // 
+            // описание_Label
+            // 
+            описание_Label.AutoSize = true;
+            описание_Label.Location = new System.Drawing.Point(214, 138);
+            описание_Label.Name = "описание_Label";
+            описание_Label.Size = new System.Drawing.Size(82, 17);
+            описание_Label.TabIndex = 4;
+            описание_Label.Text = "Описание :";
+            // 
+            // описание_TextBox
+            // 
+            this.описание_TextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.documentsBindingSource, "Описание ", true));
+            this.описание_TextBox.Location = new System.Drawing.Point(349, 135);
+            this.описание_TextBox.Multiline = true;
+            this.описание_TextBox.Name = "описание_TextBox";
+            this.описание_TextBox.Size = new System.Drawing.Size(200, 107);
+            this.описание_TextBox.TabIndex = 5;
+            // 
+            // статус_Label
+            // 
+            статус_Label.AutoSize = true;
+            статус_Label.Location = new System.Drawing.Point(214, 251);
+            статус_Label.Name = "статус_Label";
+            статус_Label.Size = new System.Drawing.Size(61, 17);
+            статус_Label.TabIndex = 6;
+            статус_Label.Text = "Статус :";
+            // 
+            // статус_ComboBox
+            // 
+            this.статус_ComboBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.documentsBindingSource, "Статус ", true));
+            this.статус_ComboBox.FormattingEnabled = true;
+            this.статус_ComboBox.Items.AddRange(new object[] {
+            "Новый",
+            "В работе",
+            "Занят"});
+            this.статус_ComboBox.Location = new System.Drawing.Point(349, 248);
+            this.статус_ComboBox.Name = "статус_ComboBox";
+            this.статус_ComboBox.Size = new System.Drawing.Size(200, 24);
+            this.статус_ComboBox.TabIndex = 7;
+            // 
+            // подписанLabel
+            // 
+            подписанLabel.AutoSize = true;
+            подписанLabel.Location = new System.Drawing.Point(214, 284);
+            подписанLabel.Name = "подписанLabel";
+            подписанLabel.Size = new System.Drawing.Size(77, 17);
+            подписанLabel.TabIndex = 8;
+            подписанLabel.Text = "Подписан:";
+            // 
+            // подписанCheckBox
+            // 
+            this.подписанCheckBox.DataBindings.Add(new System.Windows.Forms.Binding("CheckState", this.documentsBindingSource, "Подписан", true));
+            this.подписанCheckBox.Enabled = false;
+            this.подписанCheckBox.Location = new System.Drawing.Point(349, 279);
+            this.подписанCheckBox.Name = "подписанCheckBox";
+            this.подписанCheckBox.Size = new System.Drawing.Size(200, 24);
+            this.подписанCheckBox.TabIndex = 9;
+            this.подписанCheckBox.Text = "Подпись";
+            this.подписанCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(349, 326);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(199, 37);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Подписать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // SignForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(номер_документаLabel);
+            this.Controls.Add(this.номер_документаTextBox);
+            this.Controls.Add(дата_документаLabel);
+            this.Controls.Add(this.дата_документаDateTimePicker);
+            this.Controls.Add(описание_Label);
+            this.Controls.Add(this.описание_TextBox);
+            this.Controls.Add(статус_Label);
+            this.Controls.Add(this.статус_ComboBox);
+            this.Controls.Add(подписанLabel);
+            this.Controls.Add(this.подписанCheckBox);
+            this.Name = "SignForm";
+            this.Text = "SignForm";
+            this.Load += new System.EventHandler(this.SignForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.databaseDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.databaseDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.documentsBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DatabaseDataSet databaseDataSet;
+        private System.Windows.Forms.BindingSource databaseDataSetBindingSource;
+        private System.Windows.Forms.BindingSource documentsBindingSource;
+        private DatabaseDataSetTableAdapters.DocumentsTableAdapter documentsTableAdapter;
+        private DatabaseDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.TextBox номер_документаTextBox;
+        private System.Windows.Forms.DateTimePicker дата_документаDateTimePicker;
+        private System.Windows.Forms.TextBox описание_TextBox;
+        private System.Windows.Forms.ComboBox статус_ComboBox;
+        private System.Windows.Forms.CheckBox подписанCheckBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
